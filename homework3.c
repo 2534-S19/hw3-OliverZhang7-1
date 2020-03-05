71454a30b7ef4f427e07a967f1a3874a59d0df3b
Zhaomeng Zhang <Zhaomeng Zhang@DESKTOP-2KPHF5Q>
2020-03-05 15:03:42 -0500

    HW3

--- homework3.c
+++ homework3.c
@@ -10,8 +10,7 @@ int main(void)
     unsigned int count1 = 0;
 
     // TODO: Declare the variables that main uses to interact with your state machine.
-
-
+    unsigned char buttonHistory = 0xFF;
     // Stops the Watchdog timer.
     initBoard();
     // Initialize the GPIO.
@@ -35,22 +34,28 @@ int main(void)
 
         // TODO: If Timer0 has expired, increment count0.
         // YOU MUST WRITE timer0expired IN myTimer.c
-
+        if (timer0Expired())
+        {
+             count0=count0+1;
+        }
 
 
         // TODO: If Timer1 has expired, update the button history from the pushbutton value.
         // YOU MUST WRITE timer1expired IN myTimer.c
-
+        if (timer1Expired())
+        {
+            buttonHistory = (buttonHistory << 1) | checkStatus_BoosterpackS1();
+        }
 
 
         // TODO: Call the button state machine function to check for a completed, debounced button press.
         // YOU MUST WRITE THIS FUNCTION BELOW.
-
+        bool pressed = fsmBoosterpackButtonS1(buttonHistory);
 
 
         // TODO: If a completed, debounced button press has occurred, increment count1.
-
-
+        if(pressed)
+          count1=count1+1;
 
     }
 }
@@ -64,22 +69,108 @@ void initBoard()
 // Since count is an unsigned integer, you can mask the value in some way.
 void changeLaunchpadLED2(unsigned int count)
 {
-
+    turnOff_LaunchpadLED2Green();
+    turnOff_LaunchpadLED2Red();
+    turnOff_LaunchpadLED2Blue();
+    switch(count%8)
+    {
+        case 0:
+            turnOn_LaunchpadLED2Red();
+            break;
+        case 1:
+            turnOn_LaunchpadLED2Green();
+            break;
+        case 2:
+            turnOn_LaunchpadLED2Red();
+            turnOn_LaunchpadLED2Green();
+            break;
+        case 3:
+            turnOn_LaunchpadLED2Blue();
+            break;
+        case 4:
+            turnOn_LaunchpadLED2Blue();
+            turnOn_LaunchpadLED2Red();
+            break;
+        case 5:
+            turnOn_LaunchpadLED2Green();
+            turnOn_LaunchpadLED2Blue();
+            break;
+        case 6:
+            turnOn_LaunchpadLED2Green();
+            turnOn_LaunchpadLED2Blue();
+            turnOn_LaunchpadLED2Red();
+            break;
+    }
 }
 
 // TODO: Maybe the value of a count variable to a color for the Boosterpack LED
 // This is essentially a copy of the previous function, using a different LED
 void changeBoosterpackLED(unsigned int count)
 {
+    turnOff_BoosterpackLEDGreen();
+    turnOff_BoosterpackLEDRed();
+    turnOff_BoosterpackLEDBlue();
+    switch(count%8)
+    {
+     case 1:
+        turnOn_BoosterpackLEDRed();
+        break;
+     case 2:
+        turnOn_BoosterpackLEDGreen();
+        break;
+     case 3:
+         turnOn_BoosterpackLEDRed();
+         turnOn_BoosterpackLEDGreen();
+         break;
+     case 4:
+         turnOn_BoosterpackLEDBlue();
+         break;
+     case 5:
+         turnOn_BoosterpackLEDBlue();
+         turnOn_BoosterpackLEDRed();
+         break;
+     case 6:
+         turnOn_BoosterpackLEDBlue();
+         turnOn_BoosterpackLEDGreen();
+         break;
+     case 7:
+         turnOn_BoosterpackLEDRed();
+         turnOn_BoosterpackLEDBlue();
+         turnOn_BoosterpackLEDGreen();
+         break;
+    }
 
 }
 
 // TODO: Create a button state machine.
 // The button state machine should return true or false to indicate a completed, debounced button press.
-bool fsmBoosterpackButtonS1(unsigned int buttonhistory)
+bool fsmBoosterpackButtonS1(unsigned char buttonhistory)
 {
     bool pressed = false;
 
+    static enum { Pressed, Released } State = Released;
+    static unsigned char oldHistory = 0xFF;
 
+    switch (State)
+    {
+    case Released:
+        // if previous state is not all 0s and bottonhistory is all 0's then next state is pressed
+        if (oldHistory != 0x00 && buttonhistory == 0x00)
+        {
+            State = Pressed;
+            pressed = true;
+        }
+        break;
+
+    case Pressed:
+        // if previous state has zeros and bottonhistory is all 1's then next state is released
+        if (oldHistory != 0xFF && buttonhistory == 0xFF)
+        {
+            State = Released;
+        }
+        break;
+    }
+
+    oldHistory = buttonhistory;
     return pressed;
 }
